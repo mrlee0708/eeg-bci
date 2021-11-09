b4e757df3adcceaf54285dc40c2a8b46e84fbd45
Miran Lee <leemiran7461@gmail.com>
2021-11-09 15:39:18 +0900

    Commit

--- visualstimuli.m
+++ visualstimuli.m
@@ -2,15 +2,12 @@
 % 2021-11-09
 % Made by Miran Lee
 
-%% Initial Setting
+% num_trial = Integer
+% base_th = Stimulation interval (sec)
+
 % Clear the all screens and workspaces
 sca; % closes all screens
 clear all; % clear all variables
-
-% Stimulation period
-sup_th = 4; % sec
-base_th = 2; % sec
-under_th = 0.5; % sec
 
 Screen('Preference', 'SkipSyncTests', 1);
 screens = Screen ('Screens');
@@ -40,53 +37,68 @@ imastart=imread(imgstartfile, 'jpg');
 imaready=imread(imgreadyfile, 'jpg');
 imarest=imread(imgrestfile, 'jpg');
 
-% Stimulation period random
+sizeIm = [0 0 1000 1000]
+
 total_trial = 2;
-[output] = arrow_randfunction (total_trial)
+base_th = 0.5; % Stimulation interval (sec)
 
-%% Code
-[outwindow, rect] = Screen('Openwindow', screenNumber, [0 0 0]); 
-
-% Start 
-for i=1:length(total_trial)
-
-    Screen('PutImage', outwindow, imastart); % put image on screen
-    Screen('Flip',outwindow); % now visible on screen
-    WaitSecs(1);
-    Screen('FillRect',outwindow,black); % Blank screen
-    Screen('Flip',outwindow); 
-    WaitSecs(5);
-    
-    % Ready
-    Screen('PutImage', outwindow, imaready); % put image on screen
-    Screen('Flip',outwindow); % now visible on screen
-    WaitSecs(1);
-    Screen('FillRect',outwindow,black); % Blank screen
-    Screen('Flip',outwindow); 
-    WaitSecs(5);
-    
-    % Visual Simuli
-    if(output(1,i)==1)
-        imgvisual = imaleft;
-    else
-        imgvisual = imaright;
-    end
-    Screen('PutImage', outwindow, imgvisual); % put image on screen
-    Screen('Flip',outwindow); % now visible on screen
-    WaitSecs(under_th);
-    
-    % Blank screen
-    Screen('FillRect',outwindow,black); % Blank screen 
-    Screen('Flip',outwindow); 
-    WaitSecs(5);
-    
-    % Rest
-    Screen('PutImage', outwindow, imarest); % put image on screen
-    Screen('Flip',outwindow); % now visible on screen
-    WaitSecs(1);
-    Screen('FillRect',outwindow,black); % Blank screen
-    Screen('Flip',outwindow); 
-    WaitSecs(5);
+% Stimulation period random
+exp_flag=0;
+if (mod(total_trial,2)==0)
+    [output] = arrow_randfunction(total_trial);
+    exp_flag = 1;
+else  
+    disp("Please input the even number to 'total_trial'")
+    exp_flag = 0;
 end
+disp("Please input the even number to variable [total_trial]")
 
-Screen('CloseAll')
+% Main code
+if(exp_flag==1)
+    
+    [outwindow, rect] = Screen('Openwindow', screenNumber, [0 0 0]); 
+    
+    % Start 
+    for i=1:total_trial
+    
+        Screen('PutImage', outwindow, imastart); % put image on screen
+        Screen('Flip',outwindow); % now visible on screen
+        WaitSecs(1);
+        Screen('FillRect',outwindow,black); % Blank screen
+        Screen('Flip',outwindow); 
+        WaitSecs(5);
+        
+        % Ready
+        Screen('PutImage', outwindow, imaready); % put image on screen
+        Screen('Flip',outwindow); % now visible on screen
+        WaitSecs(1);
+        Screen('FillRect',outwindow,black); % Blank screen
+        Screen('Flip',outwindow); 
+        WaitSecs(5);
+        
+        % Visual Simuli
+        if(output(1,i)==1)
+            imgvisual = imaleft;
+        else
+            imgvisual = imaright;
+        end
+        Screen('PutImage', outwindow, imgvisual, [], sizeIm); % put image on screen
+        Screen('Flip',outwindow); % now visible on screen
+        WaitSecs(base_th);
+        
+        % Blank screen
+        Screen('FillRect',outwindow,black); % Blank screen 
+        Screen('Flip',outwindow); 
+        WaitSecs(5);
+        
+        % Rest
+        Screen('PutImage', outwindow, imarest); % put image on screen
+        Screen('Flip',outwindow); % now visible on screen
+        WaitSecs(1);
+        Screen('FillRect',outwindow,black); % Blank screen
+        Screen('Flip',outwindow); 
+        WaitSecs(5);
+    end
+    
+    Screen('CloseAll')
+end
